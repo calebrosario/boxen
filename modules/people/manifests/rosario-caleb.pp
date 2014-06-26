a1f55b4d28365b38e3bbc18e592fff2ee4ccaaec
Caleb Rosario <calebrosario@Calebs-MacBook-Air.local>
2014-06-26 13:20:47 -0400

    # spotify   # nmap   # virtualbox   # vagrant_manager   # vagrant   # alfred   # iterm2::stable   # chrome   # adium   # macvim   # appcleaner   # wget   # autoconf   # libtool   # pkgconfig   # pcre   # libpng   # mysql   # php   # nike_plus_connect   # skype   # arduino   # bartender   # vlc   # jmeter   # dropbox   # istatmenus4   # daisy_disk   # mysql_workbench   # utorrent   # filezilla   # caffeine   # googledrive

---
+++ modules/people/manifests/rosario-caleb.pp
@@ -0,0 +1,155 @@
+require boxen::environment
+require homebrew
+require gcc
+
+Exec {
+  group       => 'staff',
+  logoutput   => on_failure,
+  user        => $boxen_user,
+
+  path => [
+    "${boxen::config::home}/rbenv/shims",
+    "${boxen::config::home}/rbenv/bin",
+    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
+    "${boxen::config::home}/homebrew/bin",
+    '/usr/bin',
+    '/bin',
+    '/usr/sbin',
+    '/sbin'
+  ],
+
+  environment => [
+    "HOMEBREW_CACHE=${homebrew::config::cachedir}",
+    "HOME=/Users/${::boxen_user}"
+  ]
+}
+
+File {
+  group => 'staff',
+  owner => $boxen_user
+}
+
+Package {
+  provider => homebrew,
+  require  => Class['homebrew']
+}
+
+Repository {
+  provider => git,
+  extra    => [
+    '--recurse-submodules'
+  ],
+  require  => File["${boxen::config::bindir}/boxen-git-credential"],
+  config   => {
+    'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
+  }
+}
+
+Service {
+  provider => ghlaunchd
+}
+
+Homebrew::Formula <| |> -> Package <| |>
+
+node default {
+  # core modules, needed for most things
+  include dnsmasq
+  include git
+  include hub
+  include nginx
+
+  # Optional/custom modules. There are tons available at
+  # https://github.com/boxen.
+  include onepassword
+  include spotify
+  include nmap
+  include virtualbox
+  include vagrant_manager
+  include vagrant
+  include alfred
+  include iterm2::stable
+  include chrome
+  include adium
+  include macvim
+  include appcleaner
+  include wget
+  include autoconf
+  include libtool
+  include pkgconfig
+  include pcre
+  include libpng
+  include mysql
+  include php
+  include nike_plus_connect
+  include skype
+  include arduino
+  include bartender
+  include vlc
+  include jmeter
+  include dropbox
+  include istatmenus4
+  include daisy_disk
+  include mysql_workbench
+  include utorrent
+  include filezilla
+  include caffeine
+  include googledrive
+
+  include heroku
+  heroku::plugin { 'accounts':
+    source => 'ddollar/heroku-accounts'
+  }
+
+  # For the latest build of v3
+  include sublime_text
+  sublime_text::package { 'Emmet':
+    source => 'sergeche/emmet-sublime'
+  }
+
+
+  # fail if FDE is not enabled
+  if $::root_encrypted == 'no' {
+    fail('Please enable full disk encryption and try again')
+  }
+
+  # node versions
+  include nodejs::v0_6
+  include nodejs::v0_8
+  include nodejs::v0_10
+
+  # default ruby versions
+  ruby::version { '1.9.3': }
+  ruby::version { '2.0.0': }
+  ruby::version { '2.1.0': }
+  ruby::version { '2.1.1': }
+  ruby::version { '2.1.2': }
+
+  # common, useful packages
+  package {
+    [
+      'ack',
+      'findutils',
+      'gnu-tar'
+    ]:
+  }
+
+  file { "${boxen::config::srcdir}/our-boxen":
+    ensure => link,
+    target => $boxen::config::repodir
+  }
+
+  git::config::global {
+    'alias.st':   value => 'status';
+    'alias.ci':   value => 'commit';
+    'alias.co':   value => 'checkout';
+    'alias.di':   value => 'diff';
+    'alias.dc':   value => 'diff --cached';
+    'alias.lp':   value => 'log -p';
+    'color.ui':   value => 'true';
+    'user.name':  value => 'Caleb Rosario';
+    'user.email': value => 'rosario-caleb@github.com';
+  }
+
+}
+
+
